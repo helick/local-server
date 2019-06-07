52dd3a18e9ada6767988f5e45a5298cb4938836f
Evgenii Nasyrov <inasyrov@yandex.ru>
2019-06-07 15:38:46 +0100

    Add status subcommand

---
+++ src/Subcommands/StatusSubcommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Helick\LocalServer\Subcommands;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+final class StatusSubcommand
+{
+    /**
+     * The application instance.
+     *
+     * @var Application
+     */
+    private $application;
+
+    /**
+     * Create a subcommand instance.
+     *
+     * @param Application $application
+     *
+     * @return void
+     */
+    public function __construct(Application $application)
+    {
+        $this->application = $application;
+    }
+
+    /**
+     * Invoke the subcommand.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    public function __invoke(InputInterface $input, OutputInterface $output): void
+    {
+        $compose = new Process('docker-compose ps', 'vendor/helick/local-server/docker', [
+            'HELICK_PROJECT_NAME' => basename(getcwd()),
+            'VOLUME'              => getcwd(),
+        ]);
+        $compose->setTimeout(0);
+        $compose->run(function ($_, $buffer) {
+            echo $buffer;
+        });
+    }
+}
