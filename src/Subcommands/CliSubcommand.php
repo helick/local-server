2d8658b1c722cd0b55ac3e3b5fe00353020ecebe
Evgenii Nasyrov <inasyrov@yandex.ru>
2019-06-07 15:32:53 +0100

    Add cli subcommand

---
+++ src/Subcommands/CliSubcommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Helick\LocalServer\Subcommands;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class CliSubcommand
+{
+    /**
+     * The application instance.
+     *
+     * @var Application
+     */
+    private $application;
+
+    /**
+     * Create a subcommand instance.
+     *
+     * @param Application $application
+     *
+     * @return void
+     */
+    public function __construct(Application $application)
+    {
+        $this->application = $application;
+    }
+
+    /**
+     * Invoke the subcommand.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    public function __invoke(InputInterface $input, OutputInterface $output): void
+    {
+        $options = $input->getArgument('options');
+
+        $hasUrlOption = false;
+        foreach ($options as $option) {
+            if (strpos($option, '--url=') === 0) {
+                $hasUrlOption = true;
+                break;
+            }
+        }
+
+        if (!$hasUrlOption) {
+            $options[] = '--url=http://' . basename(getcwd()) . '.localtest.me/';
+        }
+
+        $hasStdin  = !posix_isatty(STDIN);
+        $hasStdout = !posix_isatty(STDOUT);
+
+        $command = sprintf(
+            'cd %s; HELICK_PROJECT_NAME=%s VOLUME=%s docker-compose exec %s -u nobody php wp %s',
+            'vendor/helick/local-server/docker',
+            basename(getcwd()),
+            getcwd(),
+            $hasStdin || $hasStdout ? '-T' : '',
+            implode(' ', $options)
+        );
+
+        passthru($command, $output);
+    }
+}
