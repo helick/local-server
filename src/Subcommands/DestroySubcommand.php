5552f2e823b80a10480b0c57ba7a668ce273d338
Evgenii Nasyrov <inasyrov@yandex.ru>
2019-06-07 15:34:45 +0100

    Add destroy subcommand

---
+++ src/Subcommands/DestroySubcommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Helick\LocalServer\Subcommands;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+final class DestroySubcommand
+{
+    /**
+     * The application instance.
+     *
+     * @var Application
+     */
+    private $application;
+
+    /**
+     * Create a subcommand instance.
+     *
+     * @param Application $application
+     *
+     * @return void
+     */
+    public function __construct(Application $application)
+    {
+        $this->application = $application;
+    }
+
+    /**
+     * Invoke the subcommand.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    public function __invoke(InputInterface $input, OutputInterface $output): void
+    {
+        $output->writeln('Destroying...');
+
+        $compose = new Process('docker-compose down -v', 'vendor/helick/local-server/docker', [
+            'HELICK_PROJECT_NAME' => basename(getcwd()),
+            'VOLUME'              => getcwd(),
+        ]);
+        $compose->run(function ($_, $buffer) {
+            echo $buffer;
+        });
+
+        $output->writeln('Destroyed.');
+    }
+}
