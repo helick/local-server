314dc7d00f3501abcf9855829b95824d904dc6f6
Evgenii Nasyrov <inasyrov@yandex.ru>
2019-06-07 15:39:41 +0100

    Add stop subcommand

---
+++ src/Subcommands/StopSubcommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Helick\LocalServer\Subcommands;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+final class StopSubcommand
+{
+    /**
+     * The application instance.
+     *
+     * @var Application
+     */
+    private $application;
+
+    /**
+     * Create a subcommand instance.
+     *
+     * @param Application $application
+     *
+     * @return void
+     */
+    public function __construct(Application $application)
+    {
+        $this->application = $application;
+    }
+
+    /**
+     * Invoke the subcommand.
+     *
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    public function __invoke(InputInterface $input, OutputInterface $output): void
+    {
+        $output->writeln('Stopping...');
+
+
+        $compose = new Process('docker-compose stop', 'vendor/helick/local-server/docker', [
+            'HELICK_PROJECT_NAME' => basename(getcwd()),
+            'VOLUME'              => getcwd(),
+        ]);
+        $compose->run(function ($_, $buffer) {
+            echo $buffer;
+        });
+
+        $output->writeln('Stopped.');
+    }
+}
